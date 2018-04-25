e666cd15afa1fb4a828d546dec8749410e209743
chigozieorunta <chigozieorunta@yahoo.com>
2018-04-25 01:43:26 +0100

    Updated jQuery script

--- uniformimages.js
+++ uniformimages.js
@@ -1,7 +1,28 @@
 $(document).ready(function() {
-    $('.unim').each(function(index, element) {   
-        var width = $(this).data("width");
+    $('.unim').each(function(index, element) {
+        //Get variables...
+        var dynamicDiv = '';
+        var source = $(this).attr("src");
         var height = $(this).attr("height");
-        $(this).parent().children('img').wrapAll('<div class="unim-container"></div>');
+        var divClass = $(this).attr("class");
+
+        //Conditionals...
+        if(parseInt(height) > 0) {
+            dynamicDiv = '<div class="'+divClass+' unim-height" style="height: '+height+'px;">';
+        } else {
+            dynamicDiv = '<div class="'+divClass+'">'; 
+        }
+
+        //Add Image URL
+        dynamicDiv += '<div class="unim-placeholder">';
+        dynamicDiv += '<div>';
+        dynamicDiv += '<div class="unim-icon"></div>';
+        dynamicDiv += '</div>';
+        dynamicDiv += '</div>';
+        dynamicDiv += '<div class="unim-img" style="background-image: url('+source+');"></div>';
+        dynamicDiv += '</div>';
+
+        //ReplaceWith dynamicDivTag
+        $(this).replaceWith(dynamicDiv);
     });
 })
