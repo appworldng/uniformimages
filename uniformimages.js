ca1c28316e003732e028c912d04d7be3dc6c5c2f
chigozieorunta <chigozieorunta@yahoo.com>
2019-02-03 21:57:11 +0100

    Updated JS files with jQuery standardization

--- uniformimages.js
+++ uniformimages.js
@@ -6,31 +6,33 @@
  * Licensed MIT <https://github.com/chigozieorunta/uniformimages/blob/master/LICENSE>
 **/
 
-$(document).ready(function() {
-    $('.unim, .unim-square, .unim-circle, .unim-portrait, .unim-landscape').each(function(index, element) {
-        //Get variables...
-        var dynamicDiv = '';
-        var source = $(this).attr("src");
-        var height = $(this).attr("height");
-        var divClass = $(this).attr("class");
-
-        //Conditionals...
-        if(parseInt(height) > 0) {
-            dynamicDiv = '<div class="'+divClass+' unim-height" style="height: '+height+'px;">';
-        } else {
-            dynamicDiv = '<div class="'+divClass+'">'; 
-        }
-
-        //Add Image URL
-        dynamicDiv += '<div class="unim-placeholder">';
-        dynamicDiv += '<div>';
-        dynamicDiv += '<div class="unim-icon"></div>';
-        dynamicDiv += '</div>';
-        dynamicDiv += '</div>';
-        dynamicDiv += '<div class="unim-img" style="background-image: url('+source+');"></div>';
-        dynamicDiv += '</div>';
-
-        //ReplaceWith dynamicDivTag
-        $(this).replaceWith(dynamicDiv);
-    });
-})
+(function($) {	
+    $(document).ready(function() {
+        $('.unim, .unim-square, .unim-circle, .unim-portrait, .unim-landscape').each(function(index, element) {
+            //Get variables...
+            var dynamicDiv = '';
+            var source = $(this).attr("src");
+            var height = $(this).attr("height");
+            var divClass = $(this).attr("class");
+    
+            //Conditionals...
+            if(parseInt(height) > 0) {
+                dynamicDiv = '<div class="'+divClass+' unim-height" style="height: '+height+'px;">';
+            } else {
+                dynamicDiv = '<div class="'+divClass+'">'; 
+            }
+    
+            //Add Image URL
+            dynamicDiv += '<div class="unim-placeholder">';
+            dynamicDiv += '<div>';
+            dynamicDiv += '<div class="unim-icon"></div>';
+            dynamicDiv += '</div>';
+            dynamicDiv += '</div>';
+            dynamicDiv += '<div class="unim-img" style="background-image: url('+source+');"></div>';
+            dynamicDiv += '</div>';
+    
+            //ReplaceWith dynamicDivTag
+            $(this).replaceWith(dynamicDiv);
+        });
+    })    
+})( jQuery );
